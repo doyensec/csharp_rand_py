3ac510d764daf2b5517f0dc8eba574203e3a58e2
Dennis Goodlett <dennis@doyensec.com>
2025-05-11 16:13:13 -0400

    Unwind variables

--- main.py
+++ main.py
@@ -246,50 +246,100 @@ def sampel_seed_4(seed):
             if SeedArray[i] < 0:
                 SeedArray[i] += MBIG
 
-    # that loop is put here, unwound
-    for i in range(1, 56):
-        m = 1 + (i + 30) % 55
-        print("SeedArray[%d] -= SeedArray[%d]" % (i, m))
-        SeedArray[i] -= SeedArray[m]
-        if SeedArray[i] < 0:
-            SeedArray[i] += MBIG
+    # the removed outer loop above is placed here unwound, with custom variables
+    sa4__1 = (SeedArray[1]  - SeedArray[32]) % MBIG
+    sa4__2 = (SeedArray[2]  - SeedArray[33]) % MBIG
+    sa4__3 = (SeedArray[3]  - SeedArray[34]) % MBIG
+    sa4__4 = (SeedArray[4]  - SeedArray[35]) % MBIG
+    sa4__5 = (SeedArray[5]  - SeedArray[36]) % MBIG
+    sa4__6 = (SeedArray[6]  - SeedArray[37]) % MBIG
+    sa4__7 = (SeedArray[7]  - SeedArray[38]) % MBIG
+    sa4__8 = (SeedArray[8]  - SeedArray[39]) % MBIG
+    sa4__9 = (SeedArray[9]  - SeedArray[40]) % MBIG
+    sa4_10 = (SeedArray[10] - SeedArray[41]) % MBIG
+    sa4_11 = (SeedArray[11] - SeedArray[42]) % MBIG
+    sa4_12 = (SeedArray[12] - SeedArray[43]) % MBIG
+    sa4_13 = (SeedArray[13] - SeedArray[44]) % MBIG
+    sa4_14 = (SeedArray[14] - SeedArray[45]) % MBIG
+    sa4_15 = (SeedArray[15] - SeedArray[46]) % MBIG
+    sa4_16 = (SeedArray[16] - SeedArray[47]) % MBIG
+    sa4_17 = (SeedArray[17] - SeedArray[48]) % MBIG
+    sa4_18 = (SeedArray[18] - SeedArray[49]) % MBIG
+    sa4_19 = (SeedArray[19] - SeedArray[50]) % MBIG
+    sa4_20 = (SeedArray[20] - SeedArray[51]) % MBIG
+    sa4_21 = (SeedArray[21] - SeedArray[52]) % MBIG
+    sa4_22 = (SeedArray[22] - SeedArray[53]) % MBIG
+    sa4_23 = (SeedArray[23] - SeedArray[54]) % MBIG
+    sa4_24 = (SeedArray[24] - SeedArray[55]) % MBIG
+    sa4_25 = (SeedArray[25] - sa4__1) % MBIG
+    sa4_26 = (SeedArray[26] - sa4__2) % MBIG
+    sa4_27 = (SeedArray[27] - sa4__3) % MBIG
+    sa4_28 = (SeedArray[28] - sa4__4) % MBIG
+    sa4_29 = (SeedArray[29] - sa4__5) % MBIG
+    sa4_30 = (SeedArray[30] - sa4__6) % MBIG
+    sa4_31 = (SeedArray[31] - sa4__7) % MBIG
+    sa4_32 = (SeedArray[32] - sa4__8) % MBIG
+    sa4_33 = (SeedArray[33] - sa4__9) % MBIG
+    sa4_34 = (SeedArray[34] - sa4_10) % MBIG
+    sa4_35 = (SeedArray[35] - sa4_11) % MBIG
+    sa4_36 = (SeedArray[36] - sa4_12) % MBIG
+    sa4_37 = (SeedArray[37] - sa4_13) % MBIG
+    sa4_38 = (SeedArray[38] - sa4_14) % MBIG
+    sa4_39 = (SeedArray[39] - sa4_15) % MBIG
+    sa4_40 = (SeedArray[40] - sa4_16) % MBIG
+    sa4_41 = (SeedArray[41] - sa4_17) % MBIG
+    sa4_42 = (SeedArray[42] - sa4_18) % MBIG
+    sa4_43 = (SeedArray[43] - sa4_19) % MBIG
+    sa4_44 = (SeedArray[44] - sa4_20) % MBIG
+    sa4_45 = (SeedArray[45] - sa4_21) % MBIG
+    sa4_46 = (SeedArray[46] - sa4_22) % MBIG
+    sa4_47 = (SeedArray[47] - sa4_23) % MBIG
+    sa4_48 = (SeedArray[48] - sa4_24) % MBIG
+    sa4_49 = (SeedArray[49] - sa4_25) % MBIG
+    sa4_50 = (SeedArray[50] - sa4_26) % MBIG
+    sa4_51 = (SeedArray[51] - sa4_27) % MBIG
+    sa4_52 = (SeedArray[52] - sa4_28) % MBIG
+    sa4_53 = (SeedArray[53] - sa4_29) % MBIG
+    sa4_54 = (SeedArray[54] - sa4_30) % MBIG
+    sa4_55 = (SeedArray[55] - sa4_31) % MBIG
+
     """
     SAMPLE Section
     """
-    ret_0 = (SeedArray[1] - SeedArray[22]) % MBIG
-    ret_1 = (SeedArray[2] - SeedArray[23]) % MBIG
-    ret_2 = (SeedArray[3] - SeedArray[24]) % MBIG
-    ret_3 = (SeedArray[4] - SeedArray[25]) % MBIG
-    ret_4 = (SeedArray[5] - SeedArray[26]) % MBIG
-    ret_5 = (SeedArray[6] - SeedArray[27]) % MBIG
-    ret_6 = (SeedArray[7] - SeedArray[28]) % MBIG
-    ret_7 = (SeedArray[8] - SeedArray[29]) % MBIG
-    ret_8 = (SeedArray[9] - SeedArray[30]) % MBIG
-    ret_9 = (SeedArray[10] - SeedArray[31]) % MBIG
-    ret10 = (SeedArray[11] - SeedArray[32]) % MBIG
-    ret11 = (SeedArray[12] - SeedArray[33]) % MBIG
-    ret12 = (SeedArray[13] - SeedArray[34]) % MBIG
-    ret13 = (SeedArray[14] - SeedArray[35]) % MBIG
-    ret14 = (SeedArray[15] - SeedArray[36]) % MBIG
-    ret15 = (SeedArray[16] - SeedArray[37]) % MBIG
-    ret16 = (SeedArray[17] - SeedArray[38]) % MBIG
-    ret17 = (SeedArray[18] - SeedArray[39]) % MBIG
-    ret18 = (SeedArray[19] - SeedArray[40]) % MBIG
-    ret19 = (SeedArray[20] - SeedArray[41]) % MBIG
-    ret20 = (SeedArray[21] - SeedArray[42]) % MBIG
-    ret21 = (SeedArray[22] - SeedArray[43]) % MBIG
-    ret22 = (SeedArray[23] - SeedArray[44]) % MBIG
-    ret23 = (SeedArray[24] - SeedArray[45]) % MBIG
-    ret24 = (SeedArray[25] - SeedArray[46]) % MBIG
-    ret25 = (SeedArray[26] - SeedArray[47]) % MBIG
-    ret26 = (SeedArray[27] - SeedArray[48]) % MBIG
-    ret27 = (SeedArray[28] - SeedArray[49]) % MBIG
-    ret28 = (SeedArray[29] - SeedArray[50]) % MBIG
-    ret29 = (SeedArray[30] - SeedArray[51]) % MBIG
-    ret30 = (SeedArray[31] - SeedArray[52]) % MBIG
-    ret31 = (SeedArray[32] - SeedArray[53]) % MBIG
-    ret32 = (SeedArray[33] - SeedArray[54]) % MBIG
-    ret33 = (SeedArray[34] - SeedArray[55]) % MBIG
+    ret_0 = (sa4__1 - sa4_22) % MBIG
+    ret_1 = (sa4__2 - sa4_23) % MBIG
+    ret_2 = (sa4__3 - sa4_24) % MBIG
+    ret_3 = (sa4__4 - sa4_25) % MBIG
+    ret_4 = (sa4__5 - sa4_26) % MBIG
+    ret_5 = (sa4__6 - sa4_27) % MBIG
+    ret_6 = (sa4__7 - sa4_28) % MBIG
+    ret_7 = (sa4__8 - sa4_29) % MBIG
+    ret_8 = (sa4__9 - sa4_30) % MBIG
+    ret_9 = (sa4_10 - sa4_31) % MBIG
+    ret10 = (sa4_11 - sa4_32) % MBIG
+    ret11 = (sa4_12 - sa4_33) % MBIG
+    ret12 = (sa4_13 - sa4_34) % MBIG
+    ret13 = (sa4_14 - sa4_35) % MBIG
+    ret14 = (sa4_15 - sa4_36) % MBIG
+    ret15 = (sa4_16 - sa4_37) % MBIG
+    ret16 = (sa4_17 - sa4_38) % MBIG
+    ret17 = (sa4_18 - sa4_39) % MBIG
+    ret18 = (sa4_19 - sa4_40) % MBIG
+    ret19 = (sa4_20 - sa4_41) % MBIG
+    ret20 = (sa4_21 - sa4_42) % MBIG
+    ret21 = (sa4_22 - sa4_43) % MBIG
+    ret22 = (sa4_23 - sa4_44) % MBIG
+    ret23 = (sa4_24 - sa4_45) % MBIG
+    ret24 = (sa4_25 - sa4_46) % MBIG
+    ret25 = (sa4_26 - sa4_47) % MBIG
+    ret26 = (sa4_27 - sa4_48) % MBIG
+    ret27 = (sa4_28 - sa4_49) % MBIG
+    ret28 = (sa4_29 - sa4_50) % MBIG
+    ret29 = (sa4_30 - sa4_51) % MBIG
+    ret30 = (sa4_31 - sa4_52) % MBIG
+    ret31 = (sa4_32 - sa4_53) % MBIG
+    ret32 = (sa4_33 - sa4_54) % MBIG
+    ret33 = (sa4_34 - sa4_55) % MBIG
 
     return [ret_0, ret_1, ret_2, ret_3, ret_4, ret_5, ret_6, ret_7, ret_8,
             ret_9, ret10, ret11, ret12, ret13, ret14, ret15, ret16, ret17,
